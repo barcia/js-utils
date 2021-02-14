c026ed0ed2176b9d45fce09365069dd4a3f96cf3
Iván Barcia <ivanbarcia@icloud.com>
2021-02-14 17:25:24 +0100

    Add ready function

--- browser.js
+++ browser.js
@@ -1,1 +1,2 @@
 export { default as createElement } from "./lib/browser/createElement/index";
+export { default as ready } from "./lib/browser/ready/index";
---
+++ lib/browser/ready/README.md
@@ -0,0 +1,1 @@
+Run event after the DOM is ready
